8b29a3f74f0def16fedea05f5c0fb7808f8785cc
megahomyak <g.megahomyak@gmail.com>
2025-07-21 18:18:18 +0500

    oaiersntoieanvuayntN FUCK FUCK FUC K SHITSHITSHITSHIT FUCK THIS SHIT. I WANT SIMPLE, NOT THIS CRAP!

--- secret_doc.sh
+++ secret_doc.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -euo pipefail
-SECRET_DOC_PATH="$1"
-TEMP="$(mktemp)"
-age -d < SECRET_DOC_PATH > TEMP
-"$EDITOR" "$TEMP"
-age -p < TEMP > SECRET_DOC_PATH
-rm "$TEMP"
+secret_doc_path="$1"
+temp="$(mktemp)"
+trap 'rm -f "$temp"' SIGTERM SIGINT EXIT
+passphrase=""
+until gpg --pinentry-mode loopback --passphrase="$passphrase" --output $file_to_edit $ecrypted_file; do
+    read -r password;
+done
+
+if [ -f "$secret_doc_path" ]; then
+    gpg --quiet --batch --yes --passphrase "$passphrase" --output "$temp" "$secret_doc_path"
+fi
+"$editor" "$temp"
+gpg --quiet --batch --yes --passphrase "$PASSPHRASE" --output "$secret_doc_path" "$temp"
+age -p < "$temp" > "$secret_doc_path"
+rm "$temp"
